282cd2a795afc138a67d4c1577ad35366342aabd
vishal-tiwari9 <vishal.urban.wheels@gmail.com>
2026-04-15 01:05:54 +0530

    Adding commit for final build fox typos fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 import { Providers } from "@/components/providers";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
